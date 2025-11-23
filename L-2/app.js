b529ff09e70550db5dd9beb3611071f9943b42d8
faruqsoft <omarfaruqngdc@gmail.com>
2025-11-23 13:42:38 +0600

    object , function export learning

---
+++ L-2/app.js
@@ -0,0 +1,6 @@
+const greet = require('./greet').greet;
+greet(4,5)// Calling the function from greet.js, akhane function hisebe asce
+//greet.greet(2,3);// object er moddhe function ta ace tai greet.greet diye call korte hobe
+// console.log(greet);// greet ekta object tai eta log korle object dekhabe, tar moddhe greet function ace.
+
+//amara object hisebe besi pathai
---
+++ L-2/greet.js
@@ -0,0 +1,9 @@
+ 
+ function greet(a,b){
+    console.log("Hello from greet function!" + (a+b) );
+}
+ //greet();//opore  log kora ace tai call korle oi log ta age dekhabe, opore return korle  call korar somoy log korte hoto
+// module.exports = greet;
+    module.exports = {
+        greet
+    }
